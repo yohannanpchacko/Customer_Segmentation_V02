64c400b71462ec4f92723790b8fefa180e6aa93f
Yohannan PC <yohannanpc@goodearth.net>
2024-12-15 18:21:21 +0530

    deployment2

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,16 +2,25 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 import sys
+import os
 
 
 def load_data():
     try:
-        sale=pd.read_csv(r"notebook\data\sale_data.csv",encoding='iso-8859-1')
+        # Get the absolute path of the current script
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        # Construct the file path relative to the repository root
+        file_path = os.path.join(base_dir, '..', '..', 'notebook', 'data', 'sale_data.csv')
+
+        # Load the CSV file
+        sale = pd.read_csv(file_path, encoding='iso-8859-1')
         sale['InvoiceDate'] = pd.to_datetime(sale['InvoiceDate'], format='%d-%m-%Y %H:%M', errors='coerce')
-        logging.info("data loading is completed")
+
+        logging.info("Data loading is completed")
         return sale
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
 def final_output_data(aggregated_df,full_cluster_df)->pd.DataFrame:
     try:
